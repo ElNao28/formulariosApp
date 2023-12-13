19536948418e21d7032ef1b2a530e1eb9ca1b2d5
ElNao <naopubg28@gmail.com>
2023-12-13 13:17:50 -0600

    formsApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
+import { AuthModule } from './auth/auth.module';
+import { ReactiveModule } from './reactive/reactive.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    ReactiveModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
